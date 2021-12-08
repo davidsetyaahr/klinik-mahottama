4b1dd6a720d4b57ed05e5a20dad1f26d36c0dd27
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2021-12-08 11:01:31 +0700

    update biaya

--- application/models/Tbl_biaya_model.php
+++ application/models/Tbl_biaya_model.php
@@ -11,14 +11,22 @@ class Tbl_biaya_model extends CI_Model
 
     public function json()
     {
-        $this->datatables->select("b.id_biaya, kb.item, b.nama_biaya, b.biaya");
+        $this->datatables->select("b.id_biaya, kb.item, b.nama_biaya, b.tipe_biaya, case when b.tipe_biaya = '1' then b.biaya else b.presentase / 100 * (select biaya from tbl_biaya where id_biaya = b.id_biaya_presentase) end as biaya");
         $this->datatables->from("tbl_biaya b");
         $this->datatables->join("tbl_kategori_biaya kb", "b.id_kategori_biaya = kb.id_kategori_biaya");
         $this->datatables->add_column('action', 
                 anchor(site_url('biaya/edit/$1'),'<i class="fa fa-pencil-square-o" aria-hidden="true"></i>', array('class' => 'btn btn-success btn-sm'))." 
-                ".anchor(site_url('biaya/delete/$1'),'<i class="fa fa-trash-o" aria-hidden="true"></i>','class="btn btn-danger btn-sm" onclick="javasciprt: return confirm(\'Are You Sure ?\')"'), 'id_tipe');
+                ".anchor(site_url('biaya/delete/$1'),'<i class="fa fa-trash-o" aria-hidden="true"></i>','class="btn btn-danger btn-sm" onclick="javasciprt: return confirm(\'Are You Sure ?\')"'), 'id_biaya');
             
         return $this->datatables->generate();
+    }
+    function getBiayaFix($not=null){
+        $this->db->select('id_biaya,nama_biaya,biaya');
+        $where = ['tipe_biaya' => '1'];
+        if($not!=null){
+            $where['id_biaya !='] = $not;
+        }
+        return $this->db->get_where('tbl_biaya',$where)->result();
     }
     public function insert($data)
     {
@@ -27,8 +35,9 @@ class Tbl_biaya_model extends CI_Model
     // get data by id
     function get_by_id($id)
     {
+        $this->db->select("b.*,case when b.tipe_biaya = '1' then b.biaya else b.presentase / 100 * (select biaya from tbl_biaya where id_biaya = b.id_biaya_presentase) end as biaya");
         $this->db->where($this->id, $id);
-        return $this->db->get($this->table)->row();
+        return $this->db->get($this->table." b")->row();
     }
     // update data
     function update($id, $data)
