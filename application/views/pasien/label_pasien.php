52265419eb8131a2eb9b1bdf6c155da2522f2170
PC Linux <greensoft@pop-os.localdomain>
2022-03-18 11:14:35 +0700

    update cetak label

--- application/views/pasien/label_pasien.php
+++ application/views/pasien/label_pasien.php
@@ -1,56 +1,98 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <style>
-        *{
-            margin : 0px;
-            padding : 0px;
+        * {
+            margin: 0px;
+            padding: 0px;
         }
-        .a5{
-            height:559.37007874px;
-            width :793.7007874px;
-            background : yellow;
-            margin : auto;
-            /* padding : .... */
+
+        .a5 {
+            height: 559.37007874px;
+            width: 793.7007874px;
+            background: yellow;
+            margin: auto;
+            padding: 4px;
         }
-        .row{
-            /* margin-bottom : ... */
+
+        .row {
+            margin-bottom: 7px;
         }
-        .row:last-child{
-            margin-bottom : 0px;
+
+        .row:last-child {
+            margin-bottom: 0px;
         }
-        .row .label{
-            height : 120.94488189px;
-            width : 204.09448819px;
-            background : white;
-            display : inline-block;
-            /* padding : ... */
-            /* margin-right : ... */
+
+        .row .label {
+            height: 120.94488189px;
+            font-size: 15px;
+            /* width: 204.09448819px; */
+            width: 245.66929134px;
+            background: white;
+            display: inline-block;
+            /* padding: 12px; */
+            margin-right: 10px;
         }
-        .row .label:last-child{
-            margin-right : 0px;
+
+        .row .label:last-child {
+            margin-right: 0px;
         }
     </style>
     <div class="a5">
         <div class="row">
             <div class="label">
-                <p>Nama: <span>Galih</span></p>
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
             </div>
             <div class="label">
-                <p>Nama: <span>Galih</span></p>
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
             </div>
             <div class="label">
-                <p>Nama: <span>Galih</span></p>
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
             </div>
         </div>
         <div class="row">
             <div class="label">
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
+            </div>
+            <div class="label">
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
+            </div>
+            <div class="label">
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
+            </div>
+            <!-- <div class="label">
                 <p>Nama: <span>Galih</span></p>
             </div>
             <div class="label">
@@ -58,8 +100,55 @@
             </div>
             <div class="label">
                 <p>Nama: <span>Galih</span></p>
+            </div> -->
+        </div>
+        <div class="row">
+            <div class="label">
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
+            </div>
+            <div class="label">
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
+            </div>
+            <div class="label">
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="label">
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
+            </div>
+            <div class="label">
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
+            </div>
+            <div class="label">
+                <p><?= $pasien->no_rekam_medis ?></p>
+                <p><?= $pasien->nama_lengkap ?></p>
+                <p><?= $pasien->tanggal_lahir ?></p>
+                <p><?= $pasien->nik ?></p>
+                <p><?= $pasien->alamat ?></p>
             </div>
         </div>
     </div>
 </body>
+
 </html>
